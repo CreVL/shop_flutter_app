b876c7a880c68af3feabfd895721b659c6c78d40
CreVL <minnibaev.vlad@mail.ru>
2022-11-25 16:48:15 +0300

    scroll right

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -37,6 +37,7 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+
       onDismissed: (direction){
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -17,23 +17,25 @@ class ProductsGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     final products = showFavorites ? productsData.favoriteItems : productsData.items;
-    return GridView.builder(
-      padding: const EdgeInsets.all(10.0),
-      itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        // create: (c) => products[i],
-        value: products[i],
-        child: ProductItem(
-          // products[i].id,
-          // products[i].title,
-          // products[i].imageUrl,
-          // products[i].price,
-        ),),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 1,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
+    return Scrollbar(
+      child: GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        itemCount: products.length,
+        itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+          // create: (c) => products[i],
+          value: products[i],
+          child: ProductItem(
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            // products[i].price,
+          ),),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
       ),
     );
   }
