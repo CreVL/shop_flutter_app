2379f9afa43e0780a3cf360208df95bb716094dc
CreVL <91658458+CreVL@users.noreply.github.com>
2023-02-22 20:52:54 +0300

    ref import Badge + hide

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,12 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Badge;
 import 'package:provider/provider.dart';
 import 'package:shop_flutter_app/screens/cart_screen.dart';
 import '../providers/cart.dart';
 import '../widgets/products_grid.dart';
 import 'package:shop_flutter_app/widgets/badge.dart';
-import '../providers/cart.dart';
-import 'package:shop_flutter_app/widgets/badge.dart';
-import './cart_screen.dart';
 import 'package:shop_flutter_app/widgets/app_box.dart';
 
 enum FilterOptions {
@@ -52,10 +49,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
               ),
             ],
           ),
-          Consumer<Cart>(builder: (_,cart, ch) => Badge(
-            child: ch,
-            value: cart.itemCount.toString(),
-          ),
+          Consumer<Cart>(builder: (_,cart, ch) => Badge(child: ch, value: cart.itemCount.toString()),
             child: IconButton(
               icon: Icon(
                 Icons.shopping_cart,
